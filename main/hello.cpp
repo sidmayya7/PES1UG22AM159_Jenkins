ca4de342a05f4f326516b0aef0e72ef0ff31f56d
sidmayya07 <sidmayya07@gmail.com>
2025-03-10 15:42:38 +0530

    correct commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << "I LOVE GO KARTING!!!!!!\n";
   return 0;
 }
